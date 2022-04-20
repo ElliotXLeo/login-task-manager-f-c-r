2fe1ed29a0e22ba0fed1a9747c32b8b728f0ae06
ElliotXLeo <elliotgaramendi@gmail.com>
2022-04-20 11:27:43 -0500

    Update styles App

--- src/App.js
+++ src/App.js
@@ -12,23 +12,25 @@ import ProtectedRoute from './pages/ProtectedRoute';
 
 function App() {
   return (
-    <AuthProvider>
-      <Router>
-        <Routes>
-          <Route path="/register" element={<Register />} />
-          <Route path="/login" element={<Login />} />
-          <Route
-            index
-            element={
-              <ProtectedRoute>
-                <Home />
-              </ProtectedRoute>
-            }
-          />
-          <Route path="*" element={<h1 className="text-8xl font-semibold text-yellow-300 text-center">404</h1>} />
-        </Routes>
-      </Router>
-    </AuthProvider>
+    <main className="flex flex-col items-center justify-center h-screen bg-slate-100">
+      <AuthProvider>
+        <Router>
+          <Routes>
+            <Route path="/register" element={<Register />} />
+            <Route path="/login" element={<Login />} />
+            <Route
+              index
+              element={
+                <ProtectedRoute>
+                  <Home />
+                </ProtectedRoute>
+              }
+            />
+            <Route path="*" element={<h1 className="text-8xl font-semibold text-yellow-300 text-center">404</h1>} />
+          </Routes>
+        </Router>
+      </AuthProvider>
+    </main>
   );
 }
 

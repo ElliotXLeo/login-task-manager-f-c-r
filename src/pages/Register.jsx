1ef0f75b9171decd48116d0ff40f240d85f8ade0
ElliotXLeo <elliotgaramendi@gmail.com>
2022-04-20 13:48:21 -0500

    Update page Register

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import Error from "../components/sections/Error";
 import useAuth from "../hooks/useAuth";
 
@@ -34,11 +34,12 @@ const Register = () => {
   };
 
   return (
-    <>
-      {error && <Error error={error} />}
-
+    <section className="flex flex-col gap-6 w-4/5 max-w-xs">
+      <h2 class="text-8xl text-center">📋</h2>
+      <h2 class="text-2xl font-semibold text-center">Registro</h2>
       <form
         onSubmit={handleSubtmit}
+        className="flex flex-col gap-4 bg-white shadow-md rounded p-4"
       >
         <input
           type="email"
@@ -47,6 +48,7 @@ const Register = () => {
           name="register-email"
           value={email}
           onChange={handleChange}
+          className="shadow border rounded p-2 focus:outline-none"
           required
         />
         <input
@@ -56,11 +58,25 @@ const Register = () => {
           name="register-password"
           value={password}
           onChange={handleChange}
+          className="shadow border rounded p-2 focus:outline-none"
           required
         />
-        <button>Registrar</button>
+        <button className="bg-blue-800 hover:bg-blue-900 py-2 rounded text-white font-bold">
+          Registrar
+        </button>
+
+        <div className="flex items-center justify-between">
+          <p className="text-sm">Ya tengo cuenta</p>
+          <Link
+            to="/login"
+            className="bg-blue-500 hover:bg-blue-600 py-2 px-4 rounded text-white text-sm font-bold">
+            Login
+          </Link>
+        </div>
       </form>
-    </>
+
+      {error && <Error error={error} />}
+    </section>
   );
 }
 
